44e04fbf6c02b9c8a3b8dbfd383ab9855e3b8995
Pravin <pravinkumar2108@gmail.com>
2024-08-08 23:01:02 +0530

    Homepage class

--- backendapi/models.py
+++ backendapi/models.py
@@ -1,1 +1,33 @@
 from django.db import models
+
+class Homepage(models.Model):
+    intro = models.CharField(
+        max_length=100, blank=True, null=True, verbose_name="User Introduction"
+    )
+    name = models.CharField(
+        max_length=100, blank=True, null=True, verbose_name="Full Name"
+    )
+    job_title = models.CharField(
+        max_length=100, blank=True, null=True, verbose_name="Role"
+    )
+    paragraph = models.TextField(blank=True, null=True, verbose_name="Introduction")
+    avatar_img = models.CharField(
+        max_length=100, blank=True, null=True, verbose_name="Google Drive Image Id"
+    )
+    HireMe_link = models.CharField(max_length=100, blank=True, null=True)
+    Resume_link = models.URLField(blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Homepage Section"
+
+        def __str__(self) -> str:
+            return self.name
+
+
+class About(models.Model):
+    title = models.CharField(
+        max_length=100,
+        blank=True,
+    )
+    subtitle = models.CharField(max_length=100, blank=True)
+    description = 
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "backendapi.apps.BackendapiConfig",
     "rest_framework",
+    "ckeditor",
 ]
 
 MIDDLEWARE = [
