2fc9e1e585a7b98b9238bfe196544840197f865a
Pravin <pravinkumar2108@gmail.com>
2024-08-09 23:55:55 +0530

    backend class created

--- backendapi/admin.py
+++ backendapi/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Homepage)
+admin.site.register(models.About)
+admin.site.register(models.LanguagesIcons)
+admin.site.register(models.MyContact)
+admin.site.register(models.Project)
+admin.site.register(models.ServicesOffred)
+admin.site.register(models.SocialMediaLinks)
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     "backendapi.apps.BackendapiConfig",
     "rest_framework",
     "ckeditor",
+    "ckeditor_uploader",
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+CKEDITOR_UPLOAD_PATH = "uploads/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
