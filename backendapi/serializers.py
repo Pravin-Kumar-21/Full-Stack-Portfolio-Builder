32edea1a350b0378f1bed03a7093c44be02be346
Pravin <pravinkumar2108@gmail.com>
2024-08-12 23:59:54 +0530

    serializers class

--- backendapi/serializers.py
+++ backendapi/serializers.py
@@ -1,8 +1,44 @@
 from rest_framework import serializers
-from .models import Homepage
+from .models import *
 
 
 class HomepageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Homepage
+        fields = "__all__"
+
+
+class AboutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = About
+        fields = "__all__"
+
+
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServicesOffred
+        fields = "__all__"
+
+
+class LanguageiconSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LanguagesIcons
+        fields = "__all__"
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = "__all__"
+
+
+class MyContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MyContact
+        fields = "__all__"
+
+
+class SocialMediaLinksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SocialMediaLinks
         fields = "__all__"
