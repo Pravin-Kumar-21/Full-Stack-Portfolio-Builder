c9c6ad9decf91e05a49c2b27de6c03d3f7425d52
Pravin Kumar <pravinkumar2108@gmail.com>
2025-09-01 06:42:15 +0530

    homepage-smallscreen view done

--- Portfolio-frontend/src/components/HomePage/HomePage.jsx
+++ Portfolio-frontend/src/components/HomePage/HomePage.jsx
@@ -2,11 +2,6 @@ import React, { useState, useEffect } from 'react';
 import './HomePage.css';
 
 
-// const get_user_image_url(img_id) => {
-//   user_img_url = `https://lh3.googleusercontent.com/d/${img_id}=w1000?authuser=0`
-//   return user_img_url;
-// }
-
 
 
 const HomePage = ({ home = {}, social = [] }) => {
@@ -26,39 +21,52 @@ const HomePage = ({ home = {}, social = [] }) => {
   return (
     <section id='homepage'>
       <div className='homepage'>
-        <div className='intro'>
-          <div className='intro-name'>
-            <h1><span className='highlight'>Hello! I’m {homeData.name} </span></h1>
-            <h2>{homeData.subcaption}</h2>
+      
+        <div className='welcome-heading'>
+          <h1>Hello! I’m {homeData.name}</h1>
+        </div>
+        
+        <div className='subcaption'>
+          <h2>{homeData.subcaption}</h2>
+        </div>
+        
+        <div className='avatar-para'>
+          <div className='para'>
             <p>{homeData.paragraph}</p>
-            <a href={homeData.HireMe_link}>
-              <button className="btn-gradient">HIRE ME</button>
-            </a>
           </div>
-
-          <div className='social-links'>
-            <div className='head-text'>Follow Me</div>
-            <br />
-            <div className="icon-links">
-              {socialData.map((platform, index) => (
-                <a
-                  key={index}
-                  href={platform.link}
-                  className="icon-link"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  title={platform.name}
-                >
-                  <i className={platform.social_icon}></i>
-                </a>
-              ))}
-            </div>
+          
+          <div className='avatar-container'>
+            <img src={homeData.avatar_img} alt="User avatar" />
           </div>
+          
         </div>
-
-        <div className='user-avatar'>
-          <img src={homeData.avatar_img}  alt="User Avatar" />
+        
+        <div className='hire-btn'>
+          <button className="btn-gradient">HIRE ME</button>
         </div>
+        
+        
+        <div className='follow-link'>
+          
+          <div className='head-text'>Follow Me</div>
+          
+          <div className="icon-links">
+            {socialData.map((platform, index) => (
+              <a
+                key={index}
+                href={platform.link}
+                className="icon-link"
+                target="_blank"
+                rel="noopener noreferrer"
+                title={platform.name}
+              >
+                <i className={platform.social_icon}></i>
+              </a>
+            ))}
+          </div>
+          
+        </div>
+        
       </div>
     </section>
   );
